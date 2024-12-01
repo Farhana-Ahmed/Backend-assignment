2d35a7de4acb6eadd0a369797d3c504d7f0b18e2
farhanak <farhana.khashiya@gmail.com>
2024-12-01 18:52:05 +0100

    POST works fine,add validation

--- src/main/kotlin/com/ahold/ctp/service/DeliveryService.kt
+++ src/main/kotlin/com/ahold/ctp/service/DeliveryService.kt
@@ -15,14 +15,20 @@ class DeliveryService(@Autowired private val deliveryRepository:DeliveryReposito
 
     //business logic to create a new delivery
     fun createDelivery(createDeliveryRequest: CreateDeliveryRequest): DeliveryResponse {
+        val startedAtDateTime = OffsetDateTime.parse(createDeliveryRequest.startedAt);
+        val delivery = Delivery(
+            vehicleId = createDeliveryRequest.vehicleId,
+            startedAt = startedAtDateTime,
+            status = createDeliveryRequest.status.name
+        );
+        val savedDelivery = deliveryRepository.save(delivery);
+        val formattedStartedAt = savedDelivery.startedAt.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
         return DeliveryResponse(
-            id= UUID.randomUUID(),
-            finishedAt = "",
-            startedAt = "",
-            vehicleId = "",
-            status = ""
-
-
+            id = savedDelivery.id!!,
+            vehicleId = savedDelivery.vehicleId,
+            startedAt = formattedStartedAt,
+            finishedAt = savedDelivery.finishedAt?.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+            status = savedDelivery.status
         )
     }
 
