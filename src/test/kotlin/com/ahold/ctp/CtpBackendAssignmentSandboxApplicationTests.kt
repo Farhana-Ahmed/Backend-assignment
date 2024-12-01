82dfedd5ca9e6ef97144a3adc1ddb7af304db605
farhanak <farhana.khashiya@gmail.com>
2024-12-01 17:23:51 +0100

    setup

---
+++ src/test/kotlin/com/ahold/ctp/CtpBackendAssignmentSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ahold.ctp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CtpBackendAssignmentSandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
